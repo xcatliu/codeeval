be093b2a0d7d9734263277a2cd8875fa0bdbb6e2
xcatliu <xcatliu@gmail.com>
2014-06-20 00:27:52 +0800

    remove sort

--- overlapping_rectangles.js
+++ overlapping_rectangles.js
@@ -11,12 +11,10 @@ require('fs').readFileSync(process.argv[2]).toString().split('\n').forEach(funct
 
 function overlapping_rectangles(rectanglesA, rectanglesB) {
     return hasCommonSection([ rectanglesA[0], rectanglesA[2] ], [ rectanglesB[0], rectanglesB[2] ]) &&
-            hasCommonSection([ rectanglesA[1], rectanglesA[3] ], [ rectanglesB[1], rectanglesB[3] ]);
+            hasCommonSection([ rectanglesA[3], rectanglesA[1] ], [ rectanglesB[3], rectanglesB[1] ]);
 }
 
 function hasCommonSection(sectionA, sectionB) {
-    sectionA.sort(function(a, b) {return a > b;});
-    sectionB.sort(function(a, b) {return a > b;});
     return sectionA[1] >= sectionB[0] && sectionB[1] >= sectionA[0];
 }
 
