c2ed8e86ec76600486ac784ab2d07ad92a7ff169
xcatliu <xcatliu@gmail.com>
2014-06-19 21:16:13 +0800

    overlapping_rectangles

---
+++ overlapping_rectangles.js
@@ -0,0 +1,24 @@
+// https://www.codeeval.com/open_challenges/70/
+
+require('fs').readFileSync(process.argv[2]).toString().split('\n').forEach(function (line) {
+    if (line === "") {
+        return;
+    }
+    var lineSplitResult = line.split(',');
+    var rectanglesA = lineSplitResult.splice(0, 4);
+    var rectanglesB = lineSplitResult;
+    console.log(isOverlapping(rectanglesA, rectanglesB));
+
+    function isOverlapping(rectanglesA, rectanglesB) {
+        return hasCommonSection([ rectanglesA[0], rectanglesA[2] ], [ rectanglesB[0], rectanglesB[2] ]) &&
+                hasCommonSection([ rectanglesA[1], rectanglesA[3] ], [ rectanglesB[1], rectanglesB[3] ]);
+    }
+
+    function hasCommonSection(sectionA, sectionB) {
+        sectionA.sort();
+        sectionB.sort();
+        return sectionA[1] >= sectionB[0] && sectionA[0] <= sectionB[1];
+    }
+
+});
+
