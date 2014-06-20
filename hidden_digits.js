8b31f8db25f10fda0172cd809e85b919d5eddf3d
xcatliu <xcatliu@gmail.com>
2014-06-20 20:50:16 +0800

    hidden_digits

---
+++ hidden_digits.js
@@ -0,0 +1,30 @@
+// https://www.codeeval.com/open_challenges/122/
+
+require('fs').readFileSync(process.argv[2]).toString().split('\n').forEach(function (line) {
+    if (line === '') {
+        return;
+    }
+    console.log(hidden_digits(line));
+});
+
+function hidden_digits(string) {
+    var result = Array.prototype.reduce.call(string, function(prev, value) {
+        return prev + getDigit(value);
+    }, '');
+    if (result === '') {
+        return 'NONE';
+    }
+    return result;
+}
+
+function getDigit(word) {
+    if (!isNaN(parseInt(word))) {
+        return word;
+    }
+    var result = word.charCodeAt(0) - 97;
+    if (result < 0 || result > 9) {
+        return '';
+    }
+    return result;
+}
+
