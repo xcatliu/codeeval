0c1740aa9f3766ff6976495b7fddfed597d71123
xcatliu <xcatliu@gmail.com>
2014-06-20 20:36:51 +0800

    not use sort

--- longest_word.js
+++ longest_word.js
@@ -7,9 +7,12 @@ require('fs').readFileSync(process.argv[2]).toString().split('\n').forEach(funct
     console.log(longest_word(line));
 });
 
-function longest_word(string) {
-    return string.split(' ').sort(function(a, b) {
-        return a.length < b.length;
-    })[0];
+function longest_word(sentence) {
+    return sentence.split(' ').reduce(function(prev, value) {
+        if (prev.length >= value.length) {
+            return prev;
+        }
+        return value;
+    }, '');
 }
 
