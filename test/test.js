6a7cab7754d9e50c05a91b8f2c6016455ff2bd45
xcatliu <xcatliu@gmail.com>
2014-08-14 16:32:27 +0800

    update test/test.js

--- test/test.js
+++ test/test.js
@@ -1,9 +1,32 @@
 var assert = require('assert');
-describe('Array', function(){
-  describe('#indexOf()', function(){
-    it('should return -1 when the value is not present', function(){
-      assert.equal(-1, [1,2,3].indexOf(5));
-      assert.equal(-1, [1,2,3].indexOf(0));
-    })
-  })
-})
+var glob = require('glob');
+var path = require('path');
+var exec = require('child_process').exec;
+var fs = require('fs');
+
+var files = glob.sync(path.resolve(__dirname, '../lib/*'));
+
+files.forEach(function(filePath) {
+    var fileName = path.basename(filePath, '.js');
+    var programPath = filePath;
+    var inputPath = path.resolve(__dirname, 'fixtures', fileName, 'input');
+    var resultPath = path.resolve(__dirname, 'fixtures', fileName, 'result');
+    describe(fileName, function() {
+        it('should equal to the result', function(done) {
+            exec('node ' + programPath + ' ' + inputPath, function(error, stdout, stderr) {
+                if (error) {
+                    return done(error);
+                }
+                var result = fs.readFileSync(resultPath, {
+                    encoding: 'utf8'
+                });
+                if (result.trim() === stdout.trim()) {
+                    done();
+                } else {
+                    done(new Error());
+                }
+            });
+        });
+    });
+});
+
