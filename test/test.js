8fcdedb27e17c2f82211ff5f89694aab3f7ac086
xcatliu <xcatliu@gmail.com>
2014-09-21 10:13:05 +0800

    update test

--- test/test.js
+++ test/test.js
@@ -10,17 +10,17 @@ files.forEach(function(filePath) {
     var fileName = path.basename(filePath, '.js');
     var programPath = filePath;
     var inputPath = path.resolve(__dirname, 'fixtures', fileName, 'input');
-    var resultPath = path.resolve(__dirname, 'fixtures', fileName, 'result');
+    var outputPath = path.resolve(__dirname, 'fixtures', fileName, 'output');
     describe(fileName, function() {
-        it('should equal to the result', function(done) {
+        it('should equal to the output', function(done) {
             exec('node ' + programPath + ' ' + inputPath, function(error, stdout, stderr) {
                 if (error) {
                     return done(error);
                 }
-                var result = fs.readFileSync(resultPath, {
+                var output = fs.readFileSync(outputPath, {
                     encoding: 'utf8'
                 });
-                if (result.trim() === stdout.trim()) {
+                if (output.trim() === stdout.trim()) {
                     done();
                 } else {
                     done(new Error());
