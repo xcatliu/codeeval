83d87d732ccaeaabbaeb6b54a96e1e986ebc1b77
xcatliu <xcatliu@gmail.com>
2015-01-09 19:03:59 +0800

    update easy/compressed_sequence.js

---
+++ easy/compressed_sequence.js
@@ -0,0 +1,25 @@
+// https://www.codeeval.com/open_challenges/128/
+
+require('fs').readFileSync(process.argv[2]).toString().split('\n').forEach(function (line) {
+    if (line === '') {
+        return;
+    }
+    var origin = line.split(' ').map(Number);
+    console.log(armstrong_numbers(origin));
+});
+
+function armstrong_numbers(origin) {
+    var i = 1;
+    var count = 1;
+    var result = [];
+    for (; i < origin.length; i++) {
+        if (origin[i] === origin[i - 1]) {
+            count++;
+        } else {
+            result.push(count, origin[i - 1]);
+            count = 1;
+        }
+    }
+    return result;
+}
+
