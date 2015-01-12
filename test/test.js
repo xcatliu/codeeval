588e35fd76585f9af186fc9e0da685930230d1d5
xcatliu <xcatliu@gmail.com>
2015-01-12 22:20:39 +0800

    update indent

--- test/test.js
+++ test/test.js
@@ -9,74 +9,74 @@ var moderateFiles = glob.sync(path.resolve(__dirname, '../moderate/*'));
 var hardFiles = glob.sync(path.resolve(__dirname, '../hard/*'));
 
 easyFiles.forEach(function(filePath) {
-    var fileName = path.basename(filePath, '.js');
-    var programPath = filePath;
-    var inputPath = path.resolve(__dirname, 'easy', fileName, 'input');
-    var outputPath = path.resolve(__dirname, 'easy', fileName, 'output');
-    describe(fileName, function() {
-        it('should equal to the output', function(done) {
-            exec('node ' + programPath + ' ' + inputPath, function(error, stdout, stderr) {
-                if (error) {
-                    return done(error);
-                }
-                var output = fs.readFileSync(outputPath, {
-                    encoding: 'utf8'
-                });
-                if (output.trim() === stdout.trim()) {
-                    done();
-                } else {
-                    done(new Error());
-                }
-            });
+  var fileName = path.basename(filePath, '.js');
+  var programPath = filePath;
+  var inputPath = path.resolve(__dirname, 'easy', fileName, 'input');
+  var outputPath = path.resolve(__dirname, 'easy', fileName, 'output');
+  describe(fileName, function() {
+    it('should equal to the output', function(done) {
+      exec('node ' + programPath + ' ' + inputPath, function(error, stdout, stderr) {
+        if (error) {
+          return done(error);
+        }
+        var output = fs.readFileSync(outputPath, {
+          encoding: 'utf8'
         });
+        if (output.trim() === stdout.trim()) {
+          done();
+        } else {
+          done(new Error());
+        }
+      });
     });
+  });
 });
 
 moderateFiles.forEach(function(filePath) {
-    var fileName = path.basename(filePath, '.js');
-    var programPath = filePath;
-    var inputPath = path.resolve(__dirname, 'moderate', fileName, 'input');
-    var outputPath = path.resolve(__dirname, 'moderate', fileName, 'output');
-    describe(fileName, function() {
-        it('should equal to the output', function(done) {
-            exec('node ' + programPath + ' ' + inputPath, function(error, stdout, stderr) {
-                if (error) {
-                    return done(error);
-                }
-                var output = fs.readFileSync(outputPath, {
-                    encoding: 'utf8'
-                });
-                if (output.trim() === stdout.trim()) {
-                    done();
-                } else {
-                    done(new Error());
-                }
-            });
+  var fileName = path.basename(filePath, '.js');
+  var programPath = filePath;
+  var inputPath = path.resolve(__dirname, 'moderate', fileName, 'input');
+  var outputPath = path.resolve(__dirname, 'moderate', fileName, 'output');
+  describe(fileName, function() {
+    it('should equal to the output', function(done) {
+      exec('node ' + programPath + ' ' + inputPath, function(error, stdout, stderr) {
+        if (error) {
+          return done(error);
+        }
+        var output = fs.readFileSync(outputPath, {
+          encoding: 'utf8'
         });
+        if (output.trim() === stdout.trim()) {
+          done();
+        } else {
+          done(new Error());
+        }
+      });
     });
+  });
 });
 
 hardFiles.forEach(function(filePath) {
-    var fileName = path.basename(filePath, '.js');
-    var programPath = filePath;
-    var inputPath = path.resolve(__dirname, 'hard', fileName, 'input');
-    var outputPath = path.resolve(__dirname, 'hard', fileName, 'output');
-    describe(fileName, function() {
-        it('should equal to the output', function(done) {
-            exec('node ' + programPath + ' ' + inputPath, function(error, stdout, stderr) {
-                if (error) {
-                    return done(error);
-                }
-                var output = fs.readFileSync(outputPath, {
-                    encoding: 'utf8'
-                });
-                if (output.trim() === stdout.trim()) {
-                    done();
-                } else {
-                    done(new Error());
-                }
-            });
+  var fileName = path.basename(filePath, '.js');
+  var programPath = filePath;
+  var inputPath = path.resolve(__dirname, 'hard', fileName, 'input');
+  var outputPath = path.resolve(__dirname, 'hard', fileName, 'output');
+  describe(fileName, function() {
+    it('should equal to the output', function(done) {
+      exec('node ' + programPath + ' ' + inputPath, function(error, stdout, stderr) {
+        if (error) {
+          return done(error);
+        }
+        var output = fs.readFileSync(outputPath, {
+          encoding: 'utf8'
         });
+        if (output.trim() === stdout.trim()) {
+          done();
+        } else {
+          done(new Error());
+        }
+      });
     });
+  });
 });
 
