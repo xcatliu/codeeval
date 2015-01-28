1084fd5eb540fc5d4ba1ad56d083b8a09a1774da
xcatliu <xcatliu@gmail.com>
2015-01-28 20:16:57 +0800

    update working_experience

--- easy/working_experience.js
+++ easy/working_experience.js
@@ -1,17 +1,23 @@
 // https://www.codeeval.com/open_challenges/139/
 
+var day = 1000 * 3600 * 24;
+
 require('fs').readFileSync(process.argv[2]).toString().split('\n').forEach(function (line) {
   if (line === '') {
     return;
   }
   var lineSplit = line.split('; ');
   var working = lineSplit.map(function(experience) {
-    return experience.split('-').map(Date);
+    return experience.split('-');
   });
   console.log(working_experience(working));
 });
 
 function working_experience(working) {
-  return working;
+  return working.map(function(one) {
+    console.log(one[0]);
+    console.log(one[1]);
+    return (Date.parse(one[1]) - Date.parse(one[0])) / day;
+  });
 }
 
