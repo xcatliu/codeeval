5e3974aaffac3d76982c80d36efd8322b0e396bb
xcatliu <xcatliu@gmail.com>
2015-01-31 21:42:57 +0800

    updaste working_experience

--- easy/working_experience/index.js
+++ easy/working_experience/index.js
@@ -1,6 +1,6 @@
 // https://www.codeeval.com/open_challenges/139/
 
-var day = 1000 * 3600 * 24;
+var year = 1000 * 3600 * 24 * 365;
 
 require('fs').readFileSync(process.argv[2]).toString().split('\n').forEach(function (line) {
   if (line === '') {
@@ -15,9 +15,7 @@ require('fs').readFileSync(process.argv[2]).toString().split('\n').forEach(funct
 
 function working_experience(working) {
   return working.map(function(one) {
-    console.log(one[0]);
-    console.log(one[1]);
-    return (Date.parse(one[1]) - Date.parse(one[0])) / day;
+    return (Date.parse(one[1]) - Date.parse(one[0])) / year;
   });
 }
 
