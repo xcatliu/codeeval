bb798f3e15276a4456df0bd1a3d9f1fdc6393a26
xcatliu <xcatliu@gmail.com>
2015-02-05 09:59:27 +0800

    add lettercase_percentage_ratio

---
+++ easy/lettercase_percentage_ratio/index.js
@@ -0,0 +1,17 @@
+// https://www.codeeval.com/open_challenges/82/
+
+require('fs').readFileSync(process.argv[2]).toString().split('\n').forEach(function (line) {
+  if (line === '') {
+    return;
+  }
+  var number = Number(line);
+  console.log(armstrong_numbers(number) ? 'True': 'False');
+});
+
+function armstrong_numbers(number) {
+  var length = number.toString().length;
+  return number === number.toString().split('').reduce(function(prev, item) {
+    return prev + Math.pow(Number(item), length);
+  }, 0);
+}
+
