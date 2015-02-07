5bcc07f04114b25b3c7fd7d0ab0ee89585325134
xcatliu <xcatliu@gmail.com>
2015-02-07 20:31:51 +0800

    modify lettercase_percentage_ratio

--- easy/lettercase_percentage_ratio/index.js
+++ easy/lettercase_percentage_ratio/index.js
@@ -1,17 +1,13 @@
-// https://www.codeeval.com/open_challenges/82/
+// https://www.codeeval.com/open_challenges/147/
 
 require('fs').readFileSync(process.argv[2]).toString().split('\n').forEach(function (line) {
   if (line === '') {
     return;
   }
-  var number = Number(line);
-  console.log(armstrong_numbers(number) ? 'True': 'False');
+  console.log(lettercase_percentage_ratio(line));
 });
 
-function armstrong_numbers(number) {
-  var length = number.toString().length;
-  return number === number.toString().split('').reduce(function(prev, item) {
-    return prev + Math.pow(Number(item), length);
-  }, 0);
+function lettercase_percentage_ratio(line) {
+  return line;
 }
 
