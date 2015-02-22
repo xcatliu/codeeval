4a2f88dc4020bf46ea16dddb4ca4ef058572f453
xcatliu <xcatliu@gmail.com>
2015-02-22 21:52:48 +0800

    update lettercase

--- easy/lettercase_percentage_ratio/index.js
+++ easy/lettercase_percentage_ratio/index.js
@@ -4,10 +4,21 @@ require('fs').readFileSync(process.argv[2]).toString().split('\n').forEach(funct
   if (line === '') {
     return;
   }
-  console.log(lettercase_percentage_ratio(line));
+  var result = lettercase_percentage_ratio(line);
+  console.log('lowercase: ' + result.lowercase.toFixed(2) + ' uppercase: ' + result.uppercase.toFixed(2));
 });
 
 function lettercase_percentage_ratio(line) {
-  return line;
+  var lowerSum = 0;
+  line.split('').forEach(function(letter) {
+    if (letter >= 'a' && letter <= 'z') {
+      lowerSum++;
+    }
+  });
+  var lowercase = lowerSum / line.length;
+  var uppercase = 1 - lowercase;
+  return {
+    lowercase: lowercase,
+    uppercase: uppercase
+  };
 }
-
